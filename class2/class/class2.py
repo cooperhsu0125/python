d935e757afe32d96a7eabfa6eef991ab8db74ed9
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2022-09-24 15:30:14 +0800

    HW

---
+++ class2/class/class2.py
@@ -0,0 +1,16 @@
+# input('提示文字')=str
+# max(a,b,c)取最大
+# int(a)變整數
+# float(a)變小數
+# str(a)變字串
+# type(a)種類
+# bool(a)True or False
+# print(a)顯示
+# len(a)長度
+# a=input('輸入年齡')
+# print('你的年齡是:'+a+'歲!')
+# r=input('輸入半徑')
+# pi =3.14
+# r=int(r)
+# area = pi * r**2
+# print(area)
