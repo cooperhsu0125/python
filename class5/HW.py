fae442b06478e4e6f5b071dfc71334ebd4c997f9
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2022-10-22 15:53:29 +0800

    class5

---
+++ class5/HW.py
@@ -0,0 +1,38 @@
+import turtle
+"""
+請使用turtle模組以及for印出以下圖形
+t0_turtle_stamp.jpg
+提示：
+turtle.home()是讓烏龜回到原點的指令
+"""
+turtle.penup()
+turtle.goto(90, 0)
+turtle.stamp()
+turtle.goto(70, 70)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(0, 90)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(-70, 70)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(-90, 0)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(-70, -70)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(0, -90)
+turtle.left(45)
+turtle.stamp()
+turtle.goto(70, -70)
+turtle.left(45)
+turtle.stamp()
+turtle.home()
+for a in range(8):
+    turtle.left(45 * a)
+    turtle.forward(100)
+    turtle.stamp()
+    turtle.home()
+turtle.done()
---
+++ class5/class.py
@@ -0,0 +1,47 @@
+from re import X
+import turtle
+# turtle.speed(1)
+# turtle.forward(1)
+# turtle.backward(1)
+# turtle.right(1)
+# turtle.left(1)
+# turtle.color('blue')
+# turtle.shape('turtle')形狀arrow,turtle,circle,square,triangle,classic
+# turtle.stamp()蓋章
+# turtle.penup()提筆
+# turtle.pendown()下筆
+# turtle.pensize(5) #線徑寬度1~10
+# turtle.pencolor("yellow") #線的顏色
+# turtle.fillcolor("yellow") #區域填滿顏色
+# turtle.begin_fill() #填滿區域設定開始
+# turtle.end_fill() #填滿區域設定結束
+# turtle.done()最後
+# for a in range(1):(從0到1(0))
+# 要回圈的程式
+# for a in range(1,8):從1到8(1,2,3,4,5,6,7)
+# for a in range(2,6,2):每次增加(2,4)
+# for a in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+# turtle.done()
+
+# turtle.speed(1000000)
+# turtle.color('blue')
+# turtle.shape('circle')
+# turtle.penup()
+# for a in range(0, 1000, 10):
+#     turtle.stamp()
+#     turtle.forward(1 + a)
+#     turtle.right(45)
+# turtle.goto(90, 90)
+# turtle.done()
+turtle.pensize(5)
+turtle.fillcolor("yellow")
+turtle.pencolor('yellow')
+turtle.begin_fill()
+
+for a in range(5):
+    turtle.forward(200)
+    turtle.right(144)
+turtle.end_fill()
+turtle.done()
