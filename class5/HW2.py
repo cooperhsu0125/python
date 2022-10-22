1463cb0428d60ccc155ebf88a343a2e7e4acb391
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2022-10-22 16:01:26 +0800

    class5

--- class5/HW2.py
+++ class5/HW2.py
@@ -3,7 +3,6 @@
 基本功能參考影片t1_turtle_time.mp4
 提示：
 turtle.clear() # 清空畫面指令
-​
 import time  # 匯入time模組
 time.sleep(1) # 延遲1秒
 """
--- class5/class.py
+++ class5/class.py
@@ -1,4 +1,3 @@
-from re import X
 import turtle
 # turtle.speed(1)
 # turtle.forward(1)
@@ -15,6 +14,7 @@ import turtle
 # turtle.fillcolor("yellow") #區域填滿顏色
 # turtle.begin_fill() #填滿區域設定開始
 # turtle.end_fill() #填滿區域設定結束
+# turtle.
 # turtle.done()最後
 # for a in range(1):(從0到1(0))
 # 要回圈的程式
@@ -35,13 +35,12 @@ import turtle
 #     turtle.right(45)
 # turtle.goto(90, 90)
 # turtle.done()
-turtle.pensize(5)
-turtle.fillcolor("yellow")
-turtle.pencolor('yellow')
-turtle.begin_fill()
-
-for a in range(5):
-    turtle.forward(200)
-    turtle.right(144)
-turtle.end_fill()
-turtle.done()
+# turtle.pensize(5)
+# turtle.fillcolor("yellow")
+# turtle.pencolor('yellow')
+# turtle.begin_fill()
+# for a in range(5):
+#     turtle.forward(200)
+#     turtle.right(144)
+# turtle.end_fill()
+# turtle.done()
