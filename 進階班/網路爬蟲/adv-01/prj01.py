6a33e38919a0a5277ec9f76eed9066a44e421527
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2023-03-04 15:21:37 +0800

    initial adv01

---
+++ 進階班/網路爬蟲/adv-01/prj01.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import random as r
+
+
+def hi():
+    Lbl.config(text='hi', fg=color[r.randint(0, 2)], bg='black')
+
+
+win = Tk()
+win.title('1')
+
+color = ['red', 'green', 'blue']
+
+Lbl = Label(win, text='', fg='white', bg='black')
+Lbl.pack()
+
+btn = Button(win, text='hi', command=hi, fg='#009EFF', bg='#646464')
+btn.pack()
+
+win.mainloop()
