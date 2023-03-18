c620f2142ce351d1059dea927acfcf4442a3d0de
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2023-03-18 15:50:19 +0800

    initial adv03

---
+++ 進階班/網路爬蟲/adv-03/prj01 copy.py
@@ -0,0 +1,36 @@
+from ttkbootstrap import *
+from PIL import ImageTk, Image
+import random as r
+import sys as s
+import os as o
+
+
+def e():
+    print('hi')
+
+
+o.chdir(s.path[0])  #設定工作目錄
+
+win = tk.Tk()
+win.title('GUI')
+win.option_add('*font', ('Helvetica', 20))  #設定預設字形
+
+style = Style(theme='vapor')  #設定主題
+style.configure('TButton', font=('Helvetica', 20))  #Tbutton是固定的, 設定按鈕字形
+
+Lbl = Label(win, text='選擇檔案:')
+Lbl.grid(row=0, column=0, sticky='E')  #row:Y  column:X  sticky靠哪個方位
+
+Lbl2 = Label(win, text='無')
+Lbl2.grid(row=0, column=1, sticky='E')
+
+btn = Button(win, text='瀏覽', command=e, style='TButton')
+btn.grid(row=0, column=2, sticky='W')
+
+btn1 = Button(win, text='顯示', command=e, style="TButton")
+btn1.grid(row=1, columnspan=3, sticky='EW')
+
+canvas = Canvas(win, width=600, height=600)
+canvas.grid(row=2, columnspan=3, sticky='EW')
+
+win.mainloop()
