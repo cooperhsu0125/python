ab55c0d47ab72aeb75c94a79d3f5f69b7a0a2842
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2023-09-02 22:53:50 +0800

    initial adv15

--- 進階班/網路爬蟲/adv-15/mod/mod.py
+++ 進階班/網路爬蟲/adv-15/mod/mod.py
@@ -114,3 +114,39 @@ def cut_video(video_path: str, beg: int, end: int):
     else:
         print("找不到影片檔案")
         return False
+
+
+def cut_video(video_path: str, beg: int, end: int):
+    if end <= beg:
+        print("結束時間必須大於開始時間")
+        return False
+    if os.path.isfile(video_path):  # 判斷影片檔案是否存在
+        clip = VideoFileClip(video_path)  # 讀取影片檔案
+        length = clip.duration  # 影片長度
+        print(f"影片長度:{length}秒")
+
+        clip = clip.subclip(beg, end)  # 切割影片
+
+        title = video_path.split(".")[0]  # 影片檔案名稱
+        new_file_name = f"{title}-{beg}-{end}.mp4"
+        clip.write_videofile(new_file_name)  # 儲存影片
+
+        print(f"影片儲存於{new_file_name}")
+
+        return True
+    else:
+        print("找不到影片檔案")
+        return False
+
+
+def merge_video(file_name_list: list, merge_file_name: str):
+    for file_name in file_name_list:
+        if not os.path.exists(file_name):
+            print(f"錯誤: {file_name}不存在")
+            return False
+    video_list = []
+    for file_name in file_name_list:
+        video_list.append(VideoFileClip(file_name))
+    final_clip = concatenate_videoclips(video_list)
+    final_clip.write_videofile(merge_file_name)
+    return True
