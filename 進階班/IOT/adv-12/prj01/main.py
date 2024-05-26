291505945cea50f4e6afe0412492a4bebb24a0ab
cooperhsu0125 <cooper.hsu.0125@gmail.com>
2024-05-26 11:00:51 +0800

    adv-12

---
+++ 進階班/IOT/adv-12/prj01/main.py
@@ -0,0 +1,16 @@
+#########################匯入模組#########################
+from machine import Pin
+import mcu
+import time
+
+#########################函式與類別定義#########################
+
+#########################宣告與設定#########################
+gpio = mcu.gpio()
+earthquake = Pin(gpio.D3, Pin.IN)
+#########################主程式#########################
+while True:
+    print(earthquake.value())
+    if earthquake.value() == 1:
+        print("Emergency")
+    time.sleep(1)
